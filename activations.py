e38e30c242db98ddb5be86f05fb9aeb67279e913
Angel-Ceballos <angel.ceballos.esp@gmail.com>
2022-06-27 02:39:37 -0500

    refactored activations and losses

---
+++ activations.py
@@ -0,0 +1,19 @@
+import numpy as np
+from activation_layer import Activation
+
+class Tanh(Activation):
+    def __init__(self) -> None:
+        tanh = lambda x: np.tanh(x)
+        tanh_prime = lambda x: 1 - np.tanh(x)**2
+        super().__init__(tanh, tanh_prime)
+
+class Sigmoid(Activation):
+    def __init__(self) -> None:
+        def sigmoid(x):
+            return 1 / (1 + np.exp(-x))
+
+        def sigmoid_prime(x):
+            s = sigmoid(x)
+            return s * (1 - s)
+
+        super().__init__(sigmoid, sigmoid_prime)
